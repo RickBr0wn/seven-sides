eb85a3562ca0294e378bfa5df3a9b42081ff4a64
Rick Brown <ricky.brown.00@gmail.com>
2021-02-01 21:03:11 +0000

    adds basic start screen

--- 7 Sides/Objects, Models and Data/ModelsAndData.swift
+++ 7 Sides/Objects, Models and Data/ModelsAndData.swift
@@ -6,9 +6,18 @@
 //
 
 import Foundation
+import SpriteKit
 
+// Side and ball information
 enum colorType {
   case Red, Orange, Pink, Blue, Yellow, Purple, Green
 }
 
 let colorWheelOrder: [colorType] = [.Red, .Orange, .Yellow, .Green, .Blue, .Purple, .Pink]
+
+var sidePositions: [CGPoint] = []
+
+// Game State
+enum gameState {
+  case beforeGame, inGame
+}
