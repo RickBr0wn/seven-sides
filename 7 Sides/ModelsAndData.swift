56fce39293e815f3e216a3eb9ecc3c7bea37d654
Rick Brown <ricky.brown.00@gmail.com>
2021-02-01 18:32:19 +0000

    adds colours to the colour wheel

---
+++ 7 Sides/ModelsAndData.swift
@@ -0,0 +1,14 @@
+//
+//  ModelsAndData.swift
+//  7 Sides
+//
+//  Created by Rick Brown on 01/02/2021.
+//
+
+import Foundation
+
+enum colorType {
+  case Red, Orange, Pink, Blue, Yellow, Purple, Green
+}
+
+let colorWheelOrder: [colorType] = [.Red, .Orange, .Yellow, .Green, .Blue, .Purple, .Pink]
